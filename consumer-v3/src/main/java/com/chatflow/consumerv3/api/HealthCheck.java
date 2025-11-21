697d3361aa2e718cf417f8519f64dd366aa472bf
Xue Han <xhan@alexandriatechnologies.onmicrosoft.com>
2025-11-21 06:55:50 -0800

    Update consumer

--- consumer-v3/src/main/java/com/chatflow/consumerv3/api/HealthCheck.java
+++ consumer-v3/src/main/java/com/chatflow/consumerv3/api/HealthCheck.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HealthController {
+public class HealthCheck {
 
   @GetMapping("/health")
   public Map<String, Object> health() {
